250bdbcf562acff2abad60ca32f3966d7a2d8567
Matt Cramblett <Cramblett5@gmail.com>
2020-07-31 11:34:10 -0400

    add spring boot structure for backend/api

---
+++ Spring/src/main/java/com/fitness/workouttracker/WorkoutTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.workouttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutTrackerApplication.class, args);
+	}
+
+}
---
+++ Spring/src/test/java/com/fitness/workouttracker/WorkoutTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.workouttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
