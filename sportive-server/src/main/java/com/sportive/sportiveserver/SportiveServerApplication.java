7f84692077439978bd16c874893d1e4d5cded5c9
Matt Cramblett <Cramblett5@gmail.com>
2020-11-13 23:16:32 -0500

    update project structure to use vue for front-end and update spring boot project

---
+++ sportive-server/src/main/java/com/sportive/sportiveserver/SportiveServerApplication.java
@@ -0,0 +1,13 @@
+package com.sportive.sportiveserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportiveServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportiveServerApplication.class, args);
+	}
+
+}
---
+++ sportive-server/src/test/java/com/sportive/sportiveserver/SportiveServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportive.sportiveserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportiveServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
